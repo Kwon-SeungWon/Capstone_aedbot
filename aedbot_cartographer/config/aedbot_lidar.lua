a1b444e945e7e5195eb8735f46f3e4230c9366af
Kwon SeungWon <ksw7384@hanyang.ac.kr>
2023-05-09 17:08:02 +0000

    Mapping 112 & Fixed nav2 yaml

--- aedbot_cartographer/config/aedbot_lidar.lua
+++ aedbot_cartographer/config/aedbot_lidar.lua
@@ -46,8 +46,8 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.min_range = 0.2
-TRAJECTORY_BUILDER_2D.max_range = 10.0
+TRAJECTORY_BUILDER_2D.min_range = 0.01
+TRAJECTORY_BUILDER_2D.max_range = 30.0
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
 -- true로 하면 맵이 안나옴;;
 TRAJECTORY_BUILDER_2D.use_imu_data = false
