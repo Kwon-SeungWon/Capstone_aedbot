07196d487a0b90514498b48d4ce8040fb3b58efb
ydoo123 <ydoo123@gmail.com>
2023-05-12 01:36:28 +0900

    Fix: build successfully

---
+++ aedbot_nodes/src/get_dest.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+from aedbot_interfaces.srv import FallDetectionToNav2
+
+
+def get_dest():
+    x, y, z, w = 1.0, 2.0, 3.0, 4.0
+    return x, y, z, w
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(FallDetectionToNav2, 'dest_val', 10)     # CHANGE
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = FallDetectionToNav2()
+        x,y,z,w = get_dest()
+        
+        msg.dest_x = x
+        msg.dest_y = y
+        msg.dest_z = z
+        msg.dest_w = w
+        
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'I pub: {msg.x}, {msg.y}, {msg.z}, {msg.w}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
