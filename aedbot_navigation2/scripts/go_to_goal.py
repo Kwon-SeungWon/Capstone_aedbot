52cc0ca97d7a3605b1e7ebf4f9aa047c54b4112c
Kwon SeungWon <ksw7384@hanyang.ac.kr>
2023-05-12 15:45:26 +0000

    Fixied go_to_goal.py add subscribe node

--- aedbot_navigation2/scripts/go_to_goal.py
+++ aedbot_navigation2/scripts/go_to_goal.py
@@ -49,6 +49,7 @@ class GotoGoal(Node):
         navigator = BasicNavigator()
 
         # Set the robot's initial pose if necessary
+        initial_poses = []
         initial_pose = PoseStamped()
         initial_pose.header.frame_id = "map"
         initial_pose.header.stamp = navigator.get_clock().now().to_msg()
@@ -59,6 +60,7 @@ class GotoGoal(Node):
         initial_pose.pose.orientation.y = 0.0
         initial_pose.pose.orientation.z = 0.0
         initial_pose.pose.orientation.w = 1.0
+        self.initial_pose = initial_poses.append(initial_pose)
         navigator.setInitialPose(initial_pose)
 
         # Activate navigation, if not autostarted. This should be called after setInitialPose()
@@ -129,6 +131,91 @@ class GotoGoal(Node):
                     navigator.followWaypoints(goal_poses)
 
         # Do something depending on the return code
+        self.result = navigator.getResult()
+        if self.result == NavigationResult.SUCCEEDED:
+            print("Goal succeeded!")
+        elif self.result == NavigationResult.CANCELED:
+            print("Goal was canceled!")
+        elif self.result == NavigationResult.FAILED:
+            print("Goal failed!")
+        else:
+            print("Goal has an invalid return status!")
+
+        navigator.lifecycleShutdown()
+
+        exit(0)
+
+    def back_to_station(self):
+        # Launch the ROS 2 Navigation Stack
+        navigator = BasicNavigator()
+        # Activate navigation, if not autostarted. This should be called after setInitialPose()
+        # or this will initialize at the origin of the map and update the costmap with bogus readings.
+        # If autostart, you should `waitUntilNav2Active()` instead.
+        # navigator.lifecycleStartup()
+
+        # Wait for navigation to fully activate. Use this line if autostart is set to true.
+        navigator.waitUntilNav2Active()
+
+        # If desired, you can change or load the map as well
+        # navigator.changeMap('/path/to/map.yaml')
+
+        # You may use the navigator to clear or obtain costmaps
+        #####################################################
+
+        # 주기적으로 local, global costmap clear (5s 마다)
+        navigator.clear_periodically_costmap()
+
+        navigator.clearAllCostmaps()  # also have clearLocalCostmap() and clearGlobalCostmap()
+        # global_costmap = navigator.getGlobalCostmap()
+        # local_costmap = navigator.getLocalCostmap()
+        ######################################################
+
+        # LET'S GO
+        nav_start = navigator.get_clock().now()
+        navigator.followWaypoints(self.initial_pose)
+
+        i = 0
+
+        while not navigator.isNavComplete():
+            #################################################
+            #                                               #
+            # Implement some code here for your application!#
+            #                                               #
+            #################################################
+
+            # Do something with the feedback
+            i = i + 1
+            feedback = navigator.getFeedback()
+            if feedback and i % 5 == 0:
+                print(
+                    "Executing current waypoint: "
+                    + str(feedback.current_waypoint + 1)
+                    + "/"
+                    + str(len(goal_poses))
+                )
+                now = navigator.get_clock().now()
+
+                # Some navigation timeout to demo cancellation
+                if now - nav_start > Duration(seconds=100000000.0):
+                    navigator.cancelNav()
+
+                # Some follow waypoints request change to demo preemption
+                if now - nav_start > Duration(seconds=500000.0):
+                    goal_pose_alt = PoseStamped()
+                    goal_pose_alt.header.frame_id = "map"
+                    goal_pose_alt.header.stamp = now.to_msg()
+                    goal_pose_alt.pose.position.x = 0.0
+                    goal_pose_alt.pose.position.y = 0.0
+                    goal_pose_alt.pose.position.z = 0.0
+                    goal_pose_alt.pose.orientation.x = 0.0
+                    goal_pose_alt.pose.orientation.y = 0.0
+                    goal_pose_alt.pose.orientation.z = 0.0
+                    goal_pose_alt.pose.orientation.w = 1.0
+                    goal_poses = [goal_pose_alt]
+                    nav_start = now
+                    navigator.followWaypoints(self.initial_pose)
+
+        # Do something depending on the return code
         result = navigator.getResult()
         if result == NavigationResult.SUCCEEDED:
             print("Goal succeeded!")
@@ -144,11 +231,16 @@ class GotoGoal(Node):
         exit(0)
 
 
-class Bridge(Node):
+class Bridge_to_Web_CPR(Node):
     def __init__(self):
         super().__init__("Bridge_Node")
 
-        if NavigationResult.SUCCEEDED == True:
+        self.subscription = self.create_subscription(
+            Bridge, "situation_end", self.listener_callback, 10  # CHANGE
+        )
+        self.subscription
+
+        if BasicNavigator.getResult == 1:
             self.pubslisher = self.create_publisher(Bridge, "arrive_dest", 10)
             timer_period = 1
             self.timer = self.create_timer(timer_period, self.timer_callback)
@@ -165,18 +257,22 @@ class Bridge(Node):
         if self.i == 5:
             exit(0)
 
+    def listener_callback(self, msg):
+        if msg.complete_cpr and msg.complete_web == True:
+            GotoGoal.back_to_station()
+
 
 def main():
     # Start the ROS 2 Python Client Library
     rclpy.init()
 
     # bridge 노드 실행
-    bridge = Bridge()
+    bridge = Bridge_to_Web_CPR()
     # Service 노드 실행
-    go_to_goal_service = GotoGoal()
+    go_to_goal = GotoGoal()
 
     rclpy.spin(bridge)
-    rclpy.spin(go_to_goal_service)
+    rclpy.spin(go_to_goal)
 
     rclpy.shutdown()
 
