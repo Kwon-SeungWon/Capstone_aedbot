826128da207ef5487c3ef8453ecd79d845fc268c
ydoo123 <ydoo123@gmail.com>
2023-05-12 23:21:20 +0900

    Test: rollback

--- aedbot_nodes/scripts/get_dest.py
+++ aedbot_nodes/scripts/get_dest.py
@@ -2,55 +2,60 @@
 
 import rclpy
 from rclpy.node import Node
-import time
-import json
 import requests
+import json
 
-from aedbot_interfaces.srv import FallDetectionToNav2
+from aedbot_interfaces.msg import FallDetectionToNav2
 
-URL = "http://130.162.152.119"
+
+URL = "http:130.162.152.119"
 
 
 def get_dest():
-    # r = requests.get(url=URL + "/get_dest")
-    # data = r.json()
+    r = requests.get(url=URL + "/get_dest")
+    data = r.json()
 
-    # x = data["x"]
-    # y = data["y"]
-    # z = data["z"]
-    # w = data["w"]
-    x, y, z, w = 3.0, 4.0, 5.0, 6.0
+    x = data["x"]
+    y = data["y"]
+    z = data["z"]
+    w = data["w"]
+
     return x, y, z, w
 
 
-def main(args=None):
-    prev_time = time.time()
+class MinimalPublisher(Node):
+    def __init__(self):
+        super().__init__("get_dest")
+        self.publisher_ = self.create_publisher(
+            FallDetectionToNav2, "dest_val", 10
+        )  # CHANGE
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = FallDetectionToNav2()
+        x, y, z, w = get_dest()
+
+        msg.dest_x = x
+        msg.dest_y = y
+        msg.dest_z = z
+        msg.dest_w = w
+
+        self.publisher_.publish(msg)
+        self.get_logger().info(
+            f"I pub: {msg.dest_x}, {msg.dest_y}, {msg.dest_z}, {msg.dest_w}"
+        )
+
+
+def main():
     rclpy.init()
 
-    node = rclpy.create_node("get_dest_node")
-    publisher = node.create_publisher(FallDetectionToNav2, "dest_val", 10)
+    minimal_publisher = MinimalPublisher()
 
-    while True:
-        now_time = time.time()
-        if abs(prev_time - now_time) > 0.1:
-            msg = FallDetectionToNav2()
+    rclpy.spin(minimal_publisher)
 
-            x, y, z, w = get_dest()
-
-            msg.dest_x = x
-            msg.dest_y = y
-            msg.dest_z = z
-            msg.dest_w = w
-
-            publisher.publish(msg)
-
-            rclpy.get_logger().info(
-                f"I pub: {msg.dest_x}, {msg.dest_y}, {msg.dest_z}, {msg.dest_w}"
-            )
-
-        prev_time = now_time
-
-    node.destroy_node()
+    minimal_publisher.destroy_node()
     rclpy.shutdown()
 
 
