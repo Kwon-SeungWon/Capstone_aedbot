bb186105d88c7d07bab079d64b5a2ee01be025cb
ydoo123 <ydoo123@gmail.com>
2023-05-22 01:49:29 +0900

    Fix: Add state to save result

--- aedbot_nodes/scripts/bridge_node.py
+++ aedbot_nodes/scripts/bridge_node.py
@@ -1,16 +1,16 @@
 #! /usr/bin/env python3
 import time  # Time library
- 
-from geometry_msgs.msg import PoseStamped # Pose with ref frame and timestamp
-from rclpy.duration import Duration # Handles time for ROS 2
-import rclpy # Python client library for ROS 2
+
+from geometry_msgs.msg import PoseStamped  # Pose with ref frame and timestamp
+from rclpy.duration import Duration  # Handles time for ROS 2
+import rclpy  # Python client library for ROS 2
 from rclpy.node import Node
 from aedbot_interfaces.msg import Bridge, FallDetectionToNav2
 from std_msgs.msg import Int32
 
+
 class Bridge_to_Web_CPR(Node):
     def __init__(self):
-        
         super().__init__("Bridge_Node")
 
         # nav2에서 bridge로 도착신호 받을 때
@@ -38,19 +38,19 @@ class Bridge_to_Web_CPR(Node):
         # msg는 Bridge의 bridge_to_nav2
         self.publisher_to_nav2 = self.create_publisher(Bridge, "arrive_dest_bridge", 10)
 
-    
-    def arrive_callback(self, msg:Bridge):
+        self.cpr_state: bool = False
+        self.web_state: bool = False
 
-        #msg = Bridge()
+    def arrive_callback(self, msg: Bridge):
+        # msg = Bridge()
 
         if msg.nav2_to_bridge == True:
-            
             print("arrive callback")
             # msg.nav2_to_bridge = True
             msg.arrive_destination = True
             self.publisher_to_CPR.publish(msg)
             print(msg)
-        
+
         else:
             msg.arrive_destination = False
             self.publisher_to_CPR.publish(msg)
@@ -58,16 +58,22 @@ class Bridge_to_Web_CPR(Node):
 
         self.get_logger().info('Publishing: "%d"' % msg.arrive_destination)
 
-
-    def end_callback(self, msg:Bridge):
-        #msg = Bridge()
+    def end_callback(self, msg: Bridge):
+        # msg = Bridge()
         print("end callback")
-        if msg.complete_cpr == 1 or msg.complete_web == True:
 
-            msg.bridge_to_nav2 = True
-            self.publisher_to_nav2.publish(msg)
-            print(msg)
+        if msg.complete_cpr == 1:
+            self.cpr_state = True
+
+        if msg.complete_web is True:
+            self.web_state = True
+
+        if self.cpr_state is True and self.web_state is True:
+            pub_msg = Bridge()
+            pub_msg.bridge_to_nav2 = True
+            self.publisher_to_nav2.publish(pub_msg)
             print("last publish success!!")
+            return None
 
 
 def main(args=None):
@@ -82,5 +88,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
